90e028822c3e69f6c482c6fcc7b070b61eab5dae
Matricarin <matricarin@gmail.com>
2023-05-24 06:08:14 +0500

    Added folders names for files locations/

--- URD.FabricationSetIssue/Program.cs
+++ URD.FabricationSetIssue/Program.cs
@@ -6,6 +6,14 @@ namespace URD.FabricationSetIssue
     {
         static void Main(string[] args)
         {
+            Model currentModel = new Model();
+            string modelPath = currentModel.GetInfo().ModelPath;
+            string mainDirectoryName = "FabricationSet";
+            string ncPlatesDirectory = "NC_plates";
+            string ncProfilesDirectory = "NC_profiles";
+            string dxfPlatesDirectory = "DXF_plates";
+            string dxfProfilesDirectory = "DXF_profiles";
+            string ncTubesDirectory = "NC_tubes";
         }
     }
 }
